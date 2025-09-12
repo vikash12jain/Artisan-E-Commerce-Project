d94dc4ef3a57a25b03d43f2f584576ac9967d644
vikash12jain <vikash12jain@gmail.com>
2025-09-12 16:43:27 +0530

    add changes

--- Server/testMongo.js
+++ Server/testMongo.js
@@ -1,7 +1,3 @@
-
-// const uri = "mongodb+srv://vikash12jain:9EPioWuavGmDtYP2@cluster0.bppekje.mongodb.net/ecomDB?retryWrites=true&w=majority&appName=Cluster0";
-// const uri =    "mongodb+srv://vikash12jain_db_ADMIN:9EPioWuavGmDtYP2@cluster0.vmycbci.mongodb.net/ecomDB?retryWrites=true&w=majority&appName=Cluster0"
-// const uri =  "mongodb+srv://vikash12jain_db_ADMIN:9EPioWuavGmDtYP2@cluster0.vmycbci.mongodb.net/ecomDB?retryWrites=true&w=majority&appName=Cluster0"  
 const mongoose = require("mongoose");
 require("dotenv").config();
 const uri = process.env.MONGO_URL
