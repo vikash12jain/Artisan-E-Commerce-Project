6cc13da79c599a72baa4dc4730ff285fe0165169
vikash12jain <vikash12jain@gmail.com>
2025-11-18 23:57:27 +0530

    minor changes

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["https://your-netlify-site.netlify.app","http://localhost:5173"],
+    origin: ["https://artisan-no-need-store.netlify.app","http://localhost:5173"],
     credentials: true,
   })
 );
